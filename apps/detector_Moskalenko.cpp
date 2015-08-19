8baa61ddd167f817530ae4a1b130409c69c803bc
Moskalenko <ss2015@ws-2k-114-11n.ccam.cmc.unn.net>
2015-08-19 17:46:30 +0300

    Detector_Moskalenko

--- apps/detector_Moskalenko.cpp
+++ apps/detector_Moskalenko.cpp
@@ -27,6 +27,7 @@ void drawDetections(const vector<Rect>& detections,
     }
 }
 
+const Scalar white(255,255, 255);
 const Scalar red(0, 0, 255);
 const Scalar green(0, 255, 0);
 const Scalar blue(255, 0, 0);
@@ -43,28 +44,77 @@ int main(int argc, char** argv)
         return 0;
     }
 
+	string detector_unn_str = "./unn_logo_cascade/cascade.xml";
+	string detector_itseez_str = "./itseez_logo_w_caption_cascade/cascade.xml";
+	string detector_opencv_str = "./opencv_logo_cascade/cascade.xml";
+
     string detector_file = parser.get<string>("detector");
     CV_Assert(!detector_file.empty());
     string image_file = parser.get<string>("image");
     string video_file = parser.get<string>("video");
     bool use_camera = parser.get<bool>("camera");
 
-    // TODO: Load detector.
+	cv::CascadeClassifier detector;
+	cv::CascadeClassifier detector_unn;
+	cv::CascadeClassifier detector_itseez;
+	cv::CascadeClassifier detector_opencv;
+	detector.load(detector_file);
+	detector_unn.load(detector_unn_str);
+	detector_itseez.load(detector_itseez_str);
+	detector_opencv.load(detector_opencv_str);
 
     if (!image_file.empty())
     {
-        // TODO: Detect objects on image.
-
+		Mat image;
+		image = cv::imread(image_file);
+		vector<Rect> result;
+		detector.detectMultiScale(image,result);
+		drawDetections(result,red,image);
+		imshow("Image", image);
+		waitKey(0);   
     }
     else if (!video_file.empty())
     {
-        // TODO: Detect objects on every frame of a video.
-
+		namedWindow("Video",1);
+		cv::VideoCapture cap(video_file);
+		CV_Assert(cap.isOpened());
+		Mat image;
+		cap >> image;
+		while (cap.isOpened())
+		{
+			cap >> image;
+			vector<Rect> result;
+			detector.detectMultiScale(image,result);
+			drawDetections(result,red,image);
+			imshow("Video", image);
+			if(waitKey(30) >= 0) break;
+		};
     }
     else if (use_camera)
     {
-        // TODO: Detect objects on a live video stream from camera.
-
+		namedWindow("Video",1);
+        cv::VideoCapture cap = cv::VideoCapture(0);
+		cap.open(0);
+		Mat image;
+		cap >> image;
+		for(;;)
+		{
+			cap >> image;
+			vector<Rect> result;
+			vector<Rect> resultUnn;
+			vector<Rect> resultOpencv;
+			vector<Rect> resultItseez;
+			detector.detectMultiScale(image,result);
+			detector_unn.detectMultiScale(image,resultUnn);
+			detector_opencv.detectMultiScale(image,resultOpencv);
+			detector_itseez.detectMultiScale(image,resultItseez);
+			drawDetections(resultUnn,red,image);
+			drawDetections(resultOpencv,blue,image);
+			drawDetections(resultItseez,green,image);
+			drawDetections(result,white,image);
+			imshow("Video", image);
+			if(waitKey(30) >= 0) break;
+		};
     }
     else
     {
